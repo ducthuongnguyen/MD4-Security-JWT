40da06fb885ccec7393101190f2a57b8e42b7605
ducthuongnguyen <ducthuong.nguyen241@gmail.com>
2022-06-19 15:53:19 +0700

    loi chua chay duoc

---
+++ src/main/java/com/codegym/DemoJwtSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJwtSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJwtSecurityApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/codegym/service/RoleService.java
@@ -0,0 +1,11 @@
+package com.codegym.service;
+
+import com.codegym.model.Role;
+
+public interface RoleService {
+    Iterable<Role> findAll();
+
+    void save(Role role);
+
+    Role findByName(String name);
+}
